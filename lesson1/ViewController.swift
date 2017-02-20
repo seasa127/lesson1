bbe237e0698c356d39099f03fef08481da241640
yuling <seasa127@gmail.com>
2017-02-20 22:58:26 +0800

    20170220 adding app icon

---
+++ lesson1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by  Lily Hsu on 2/13/17.
+//  Copyright © 2017  Lily Hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
